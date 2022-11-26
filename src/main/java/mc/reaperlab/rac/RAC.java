08293b05bc11e036f8a98a16cd96f47715bda6ca
ReaperLab <101026822+ReaperLab@users.noreply.github.com>
2022-11-25 23:01:45 -0500

    Web

---
+++ src/main/java/mc/reaperlab/rac/RAC.java
@@ -0,0 +1,17 @@
+package mc.reaperlab.rac;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RAC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
